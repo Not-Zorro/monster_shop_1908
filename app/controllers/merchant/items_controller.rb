c08f38ea514f8c76f8c9efa85df1381d21b6ef44
Alice Post <alicep2322@gmail.com>
2019-10-27 17:18:14 -0600

    Remove error flash and add order.update conditional when items are fulfilled
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>
    Co-Authored-By: null <DavidBarriga-Gomez@users.noreply.github.com>

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -7,16 +7,13 @@ class Merchant::ItemsController < Merchant::BaseController
   def fulfill
     item = Item.find(params[:item_id])
     item_order = item.item_orders.find_by(order_id: params[:order_id])
-    if item_order.status != 'fulfilled'
-      item_order.update(status: 'fulfilled')
-      new_inventory = item.inventory - item_order.quantity
-      item.update(inventory: new_inventory)
-      flash[:success] = "You have fulfilled #{item.name}."
-      redirect_to "/merchant/orders/#{params[:order_id]}"
-    else
-      flash[:error] = "You have already fulfilled this item."
-      redirect_to "/merchant/orders/#{params[:order_id]}"
-    end
+    order = Order.find(params[:order_id])
+    item_order.update(status: 'fulfilled')
+    new_inventory = item.inventory - item_order.quantity
+    item.update(inventory: new_inventory)
+    order.update(status: 'packaged') if order.all_items_fulfilled?
+    flash[:success] = "You have fulfilled #{item.name}."
+    redirect_to "/merchant/orders/#{params[:order_id]}"
   end
 
 end

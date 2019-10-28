56e9cb0e4a972b68d228de710775f4a4136b3302
David Lopez <ldavid.x.lopezl@gmail.com>
2019-10-28 14:41:17 -0600

    Add test for having a button to activate item
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: null <DavidBarriga-Gomez@users.noreply.github.com>

--- spec/features/users/merchant/items/index_spec.rb
+++ spec/features/users/merchant/items/index_spec.rb
@@ -11,6 +11,9 @@ describe 'as a merchant admin' do
       @pull_toy = @chester_the_merchant.items.create!(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
       @dog_bone = @chester_the_merchant.items.create!(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
 
+      @ball = @chester_the_merchant.items.create!(name: "Tennis ball", description: "It's Green!", price: 1, image: "https://www.salemacademycs.org/wp-content/uploads/Tennis-balls.jpg", inventory: 500, active?: false)
+
+
     end
     it 'click button to deactivate item' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_admin)
@@ -39,6 +42,32 @@ describe 'as a merchant admin' do
 
       within "#item-#{@pull_toy.id}" do
         expect(page).to_not have_button('Deactivate')
+      end
+    end
+
+    it 'click button to activate item' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_admin)
+      visit '/merchant/items'
+
+      within "#item-#{@ball.id}" do
+        click_button 'Activate'
+      end
+
+      expect(current_path).to eq('/merchant/items')
+
+      expect(page).to have_content("#{@ball.name} is for sale")
+
+      within "#item-#{@ball.id}" do
+        expect(page).to have_content('Active')
+      end
+    end
+
+    it "Can not activate or see button as a merchant_employee" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_employee)
+      visit '/merchant/items'
+
+      within "#item-#{@ball.id}" do
+        expect(page).to_not have_button('Activate')
       end
     end
   end

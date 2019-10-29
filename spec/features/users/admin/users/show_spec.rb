a4c767297903c4384551c7c9bf904b148b6a5d1b
David Lopez <ldavid.x.lopezl@gmail.com>
2019-10-29 15:22:47 -0600

    Add test for /admin/users/:id show page
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: null <DavidBarriga-Gomez@users.noreply.github.com>
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>

---
+++ spec/features/users/admin/users/show_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe "Admin users index page" do
+  before(:each) do
+
+    @admin = User.create!(name: 'Admin Foxy', address: '123 Fake St', city: 'Denver', state: 'Colorado', zip: 80111, email: 'admin@admin.com', password: 'password', role: 3)
+    @user = User.create!(name: 'Customer Sally', address: '123 Fake St', city: 'Denver', state: 'Colorado', zip: 80111, email: 'user@user.com', password: 'password' )
+    @merchant_employee = User.create!(name: 'Drone', address: '123 Fake St', city: 'Denver', state: 'Colorado', zip: 80111, email: 'employee@employee.com', password: 'password', role: 1 )
+    @merchant_admin = User.create!(name: 'Boss', address: '123 Fake St', city: 'Denver', state: 'Colorado', zip: 80111, email: 'boss@boss.com', password: 'password', role: 2 )
+  end
+
+  it 'can display a users show page without edit link' do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+    visit '/admin/users'
+
+    within "#user-#{@user.id}" do
+      click_link(@user.name)
+    end
+
+    expect(current_path).to eq("/admin/users/#{@user.id}")
+
+    within '.user-profile' do
+      expect(page).to have_content("Name: #{@user.name}")
+      expect(page).to have_content("Address: #{@user.address}")
+      expect(page).to have_content("City: #{@user.city}")
+      expect(page).to have_content("State: #{@user.state}")
+      expect(page).to have_content("Zip: #{@user.zip}")
+      expect(page).to have_content("Email: #{@user.email}")
+    end
+    
+    expect(page).to_not have_link('Edit Profile')
+  end
+end
+
+# As an admin user
+# When I visit a user's profile page ("/admin/users/5")
+# I see the same information the user would see themselves
+# I do not see a link to edit their profile

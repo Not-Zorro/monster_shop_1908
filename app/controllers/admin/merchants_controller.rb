07f5c5431fc9c35a058f637a423fabe31505cc29
David Gomez <davdg25@gmail.com>
2019-10-29 16:15:45 -0600

    Add flash to existing actions
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -7,12 +7,14 @@ class Admin::MerchantsController < Admin::BaseController
   def disable
     merchant = Merchant.find(params[:id])
     merchant.update(active?: false)
+    flash[:success] = "#{merchant.name} has been disabled"
     redirect_to '/merchants'
   end
 
   def enable
     merchant = Merchant.find(params[:id])
     merchant.update(active?: true)
+    flash[:success] = "#{merchant.name} has been enabled"
     redirect_to '/merchants'
   end
 end

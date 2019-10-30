729321835b6be4794009b9c00fdd4fb03ada6965
Alice Post <alicep2322@gmail.com>
2019-10-30 12:13:35 -0600

    Move merchants edit, new, and delete functionality into the admin space
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>
    Co-Authored-By: null <DavidBarriga-Gomez@users.noreply.github.com>

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -4,6 +4,34 @@ class Admin::MerchantsController < Admin::BaseController
     @merchant = Merchant.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    merchant = Merchant.create(merchant_params)
+    if merchant.save
+      redirect_to merchants_path
+    else
+      flash[:error] = merchant.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:id])
+    @merchant.update(merchant_params)
+    if @merchant.save
+      redirect_to "/merchants/#{@merchant.id}"
+    else
+      flash[:error] = @merchant.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
   def disable
     merchant = Merchant.find(params[:id])
     merchant.update(active?: false)
@@ -20,4 +48,16 @@ class Admin::MerchantsController < Admin::BaseController
     flash[:success] = "#{merchant.name} has been enabled"
     redirect_to '/merchants'
   end
+
+  def destroy
+    Merchant.destroy(params[:id])
+    redirect_to '/merchants'
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:name,:address,:city,:state,:zip)
+  end
+
 end

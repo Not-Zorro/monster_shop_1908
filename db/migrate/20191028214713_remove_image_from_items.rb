ccbb020015784442c42593f10a5fba3b3218a2ec
David Lopez <ldavid.x.lopezl@gmail.com>
2019-10-30 15:53:04 -0600

    Remove deletion migrations messing with heroku

--- db/migrate/20191028214713_remove_image_from_items.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImageFromItems < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :items, :image, :string
-  end
-end
--- db/migrate/20191029173518_remove_status_from_orders.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStatusFromOrders < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :orders, :status, :string
-  end
-end

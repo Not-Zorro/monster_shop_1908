1a1c035c8ea822f7733a0cb19df37cba3d13b4fc
Alice Post <alicep2322@gmail.com>
2019-10-30 13:12:03 -0600

    Update new merchant test to be within the admin role

---
+++ spec/features/users/admin/merchants/new_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant new page', type: :feature do
+  describe 'As an admin' do
+    before(:each) do
+      @chester_the_merchant = Merchant.create!(name: "Chester's Shop", address: '456 Terrier Rd.', city: 'Richmond', state: 'VA', zip: 23137)
+      @bike_shop = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+      @user = User.create!(name: 'Customer Sally', address: '123 Fake St', city: 'Denver', state: 'Colorado', zip: 80111, email: 'user@user.com', password: 'password' )
+      @merchant_employee = @chester_the_merchant.users.create!(name: 'Drone', address: '123 Fake St', city: 'Denver', state: 'Colorado', zip: 80111, email: 'employee@employee.com', password: 'password', role: 1 )
+      @merchant_admin = @chester_the_merchant.users.create!(name: 'Boss', address: '123 Fake St', city: 'Denver', state: 'Colorado', zip: 80111, email: 'boss@boss.com', password: 'password', role: 2 )
+      @admin = User.create!(name: 'Admin Foxy', address: '123 Fake St', city: 'Denver', state: 'Colorado', zip: 80111, email: 'admin@admin.com', password: 'password', role: 3)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+    end
+
+    it 'I can create a new merchant' do
+      visit '/admin/merchants/new'
+
+      name = "Sal's Calz(ones)"
+      address = '123 Kindalikeapizza Dr.'
+      city = "Denver"
+      state = "CO"
+      zip = 80204
+
+      fill_in :name, with: name
+      fill_in :address, with: address
+      fill_in :city, with: city
+      fill_in :state, with: state
+      fill_in :zip, with: zip
+
+      click_button "Create Merchant"
+
+      new_merchant = Merchant.last
+
+      expect(current_path).to eq('/merchants')
+      expect(page).to have_content(name)
+      expect(new_merchant.name).to eq(name)
+      expect(new_merchant.address).to eq(address)
+      expect(new_merchant.city).to eq(city)
+      expect(new_merchant.state).to eq(state)
+      expect(new_merchant.zip).to eq(zip)
+    end
+
+    it 'I cant create a merchant if all fields are not filled in' do
+      visit '/admin/merchants/new'
+
+      name = "Sal's Calz(ones)"
+      address = ''
+      city = "Denver"
+      state = ""
+      zip = 80204
+
+      fill_in :name, with: name
+      fill_in :address, with: address
+      fill_in :city, with: city
+      fill_in :state, with: state
+      fill_in :zip, with: zip
+
+      click_button "Create Merchant"
+
+      expect(page).to have_content("Address can't be blank and State can't be blank")
+      expect(page).to have_button("Create Merchant")
+    end
+
+  end
+end
